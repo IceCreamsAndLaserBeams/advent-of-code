4605eede874def65ce8a06a9e72cced6be17934e
Leo Sammarco <leo.sammarco@booking.com>
2024-12-01 14:38:18 +0000

    switched to typescript, might regret this

--- 2024/01/02.ts
+++ 2024/01/02.ts
@@ -2,13 +2,13 @@ import * as path from "path";
 import readFile from "../../util/readFile";
 
 const originalPairStrings = readFile(path.join(__dirname, 'input.txt'));
-const leftList = [];
-const rightList = [];
+const leftList: number[] = [];
+const rightList: number[] = [];
 
 originalPairStrings.forEach(ops => {
     const pair = ops.split('   ');
-    leftList.push(pair[0]);
-    rightList.push(pair[1]);
+    leftList.push(parseInt(pair[0]));
+    rightList.push(parseInt(pair[1]));
 });
 
 const similarityScore = leftList.reduce((prev, leftLocId) => {
---
+++ README.md
@@ -0,0 +1,22 @@
+# Advent of Code
+
+### Solutions* to the advent of code challenges written in javascript.
+
+*Not necessarily the smartest or most performant.
+
+## Setup
+
+Install [bun.sh](https://bun.sh) if you don't already have it:
+```shell
+curl -fsSL https://bun.sh/install | bash
+```
+
+Install types:
+```shell
+bun install
+```
+
+Watch your file:
+```shell
+bun --watch 2024/01/01.ts
+```
