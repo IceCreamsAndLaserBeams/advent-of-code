3e366803f38bff4fa659a4c4b698d82dc0114122
Leo Sammarco <leo.sammarco@booking.com>
2024-12-01 14:40:28 +0000

    forgot to convert readFile.js to typescript

--- 2024/01/02.ts
+++ 2024/01/02.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import readFile from "../../util/readFile";
+import readFile from "../../util/readFile.ts";
 
 const originalPairStrings = readFile(path.join(__dirname, 'input.txt'));
 const leftList: number[] = [];
--- util/readFile.ts
+++ util/readFile.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const readFile = (filePath) => {
+const readFile = (filePath: string) => {
     const data = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' });
     let dataArr = data.split('\n');
     dataArr.pop();
