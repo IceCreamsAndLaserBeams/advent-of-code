1b9c5d49137a01768219209367ac77fe5a2e01bb
Leo Sammarco <leo.sammarco@booking.com>
2024-12-02 09:55:00 +0000

    solution for day 2 pt.1

---
+++ 2024/02/01.ts
@@ -0,0 +1,21 @@
+import readFile from "../../util/readFile.ts";
+import path from "path";
+
+const originalReportStrings = readFile(path.join(__dirname, "input.txt"));
+const reports = originalReportStrings.map((rs) =>
+  rs.split(" ").map((s) => parseInt(s)),
+);
+
+const validReports = reports.filter((report) => {
+  const isAcceptableRange = report.every((x, i) => {
+    let diff = Math.abs(x - report[i - 1]);
+    return i === 0 || (diff >= 1 && diff <= 3);
+  });
+
+  const isAscending = report.every((x, i) => i === 0 || x > report[i - 1]);
+  const isDescending = report.every((x, i) => i === 0 || x < report[i - 1]);
+
+  return isAcceptableRange && (isAscending || isDescending);
+});
+
+console.log(validReports.length);
