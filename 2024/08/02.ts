2f8ea1f4b2fa132d3b99f45e24167ced5fc6de78
Leo Sammarco <leo.sammarco@booking.com>
2024-12-08 23:54:19 +0000

    added solutions for day 8 pt1 and pt2

---
+++ 2024/08/02.ts
@@ -0,0 +1,61 @@
+import { readFileLines } from "../../util/readFile.ts";
+import path from "path";
+
+type Node = [number, number];
+type Antenna = {
+  [key: string]: [Node?];
+};
+type AntiNodes = {
+  [key: string]: boolean;
+};
+
+const matrix = readFileLines(path.join(__dirname, "input.txt")).map((r) =>
+  r.split(""),
+);
+
+const antennas: Antenna = {},
+  antiNodes: AntiNodes = {},
+  antiNodesResonance: AntiNodes = {};
+
+const withinMatrix = ([x, y]: Node) => matrix[x] && matrix[x][y];
+
+const registerAntiNode = (
+  node: Node,
+  nextNodeDistance: number[],
+  level: number,
+) => {
+  if (!withinMatrix(node)) return;
+
+  let [x, y] = node,
+    [dx, dy] = nextNodeDistance;
+  if (level === 1) antiNodes[`${x},${y}`] = true;
+  antiNodesResonance[`${x},${y}`] = true;
+  registerAntiNode([x + dx, y + dy], [dx, dy], ++level);
+};
+
+const registerAntiNodesResonance = (current: Node, next: Node) => {
+  let [cx, cy] = current,
+    [nx, ny] = next;
+  const [dx, dy] = [nx - cx, ny - cy];
+
+  registerAntiNode([cx, cy], [-dx, -dy], 0);
+  registerAntiNode([nx, ny], [dx, dy], 0);
+};
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j] !== ".") (antennas[matrix[i][j]] ??= []).push([i, j]);
+  }
+}
+
+Object.keys(antennas)
+  .filter((key) => antennas[key]?.length > 1)
+  .forEach((key) => {
+    for (let i = 0; i < antennas[key].length - 1; i++) {
+      for (let j = i + 1; j < antennas[key].length; j++) {
+        registerAntiNodesResonance(antennas[key][i]!, antennas[key][j]!);
+      }
+    }
+  });
+
+console.log(Object.keys(antiNodesResonance).length);
